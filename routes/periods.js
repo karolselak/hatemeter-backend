0924e5b690046337157dcf1dc19383a60842cc9d
Piotr Dybowski <38506213+Piter97@users.noreply.github.com>
2019-05-25 09:19:41 +0200

    deleted merged text

--- routes/periods.js
+++ routes/periods.js
@@ -2,43 +2,38 @@ const {Period} = require('../models/period');
 const express = require('express');
 const router = express.Router();
 
-<<<<<<< HEAD
 router.post('/', async(req,res) => {
-=======
+
 router.post('/:agentId', async(req,res) => {
->>>>>>> 273d0858d152334c780f22b170bc052e9f93603a
+
     const startTime = new Date();
 
     var lastPeriod = await Period.findOne({endTime: null});
     lastPeriod.endTime = startTime;
-<<<<<<< HEAD
+
     lastPeriod.endEsteem = req.body.endEsteem;
     res.send(lastPeriod);
-=======
->>>>>>> 273d0858d152334c780f22b170bc052e9f93603a
 
     var period = new Period({
         agentId: req.bady.agentId,
         periodId: req.body.periodId,
         ifBreak: req.body.ifBreak,
         startTime: startTime,
-<<<<<<< HEAD
+
         endTime: null,
         startEsteem: req.body.startEsteem,
         endEsteem: null
-=======
+
         endTime: null
->>>>>>> 273d0858d152334c780f22b170bc052e9f93603a
+
     })
 
     period = await period.save();
 
-<<<<<<< HEAD
     res.send(period);
 });
 
 module.exports = router
-=======
     res.send(lastPeriod);
     res.send(period);
 });
@@ -46,4 +41,4 @@ module.exports = router
 router.put('/:agentId', async (req, res) => {
 
 });
->>>>>>> 273d0858d152334c780f22b170bc052e9f93603a
+
